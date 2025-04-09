f13c0262da931f0e960021fd75a923032ab8898c
Richard <richard.kovi.07@gmail.com>
2025-04-09 13:13:25 +0200

    asdd

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,6 +6,10 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using Spectre.Console;
+
 
 namespace Project
 {
@@ -27,23 +31,57 @@ namespace Project
             var marginx = 10;
             var marginy = 5;
 
-            Default(marginx,marginy);
+            Default(marginx, marginy);
 
-            var YearCalendar = new RenderYears(marginx, marginy);
-            var a1 = await Task.Run(() => YearCalendar.Render());
+            string filePath = "calendar_data.json";
+
+            var calendarData = CalendarStorage.LoadAllData(filePath); //-> Ez egy Dictionary HA letezik akkor visszaadja az elmentettet ha nem akkor egy ujat ad vissza
+
+            //var asd = new Calendar(2025, 4, 9);
+            //asd.Culture("hu-HU");
+            //asd.AddCalendarEvent(new DateTime(2025, 4, 9), Style.Parse("yellow"));
+            //AnsiConsole.Write(asd);
+
             
+            // SAVE
+            CalendarStorage.SaveAllData(calendarData, filePath);
+        }
+    }
+    public class DailyData
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public List<string> ToDo { get; set; }
+    }
 
-            var MonthCalendar = new RenderMonths(marginx, marginy);
-            var a2 = await Task.Run(() => MonthCalendar.Render());
+    public static class CalendarStorage
+    {
+        public static void SaveAllData(Dictionary<string, DailyData> allData, string filePath)
+        {
+            var json = JsonSerializer.Serialize(allData,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                }
+            );
+            File.WriteAllText(filePath, json);
+        }
 
-            var DayCalendar = new RenderDays(marginx, marginy, YearCalendar.SelectedYear, MonthCalendar.SelectedMonth);
-            var a3 = await Task.Run(() => DayCalendar.Render());
+        public static Dictionary<string, DailyData> LoadAllData(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new Dictionary<string, DailyData>();
+            }
 
-            Console.WriteLine(a1);
-            Console.WriteLine(a2);
-            Console.WriteLine(a3);
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Dictionary<string, DailyData>>(json);
+        }
 
-            Thread.Sleep(-1);
+        public static string GetDateKey(int year, int month, int day)
+        {
+            return $"{year:D4}-{month:D2}-{day:D2}";
         }
     }
 }
